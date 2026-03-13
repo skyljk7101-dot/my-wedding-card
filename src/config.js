e4528e490f7e131bcb438632ef52cd97979097e9
JAEGI <skyljk7101@gmail.com>
2026-03-13 22:34:45 +0900

    Remove photo 12-1 from gallery

--- src/config.js
+++ src/config.js
@@ -30,7 +30,7 @@ export const INVITE = {
     "/assets/1.jpg", "/assets/2.jpg", "/assets/3.jpg", "/assets/4.jpg",
     "/assets/5.jpg", "/assets/6.jpg", "/assets/7.jpg", "/assets/8.jpg",
     "/assets/8-1.jpg", "/assets/8-2.jpg", "/assets/9.jpg",
-    "/assets/10.jpg", "/assets/11.jpg", "/assets/12.jpg", "/assets/12-1.jpg",
+    "/assets/10.jpg", "/assets/11.jpg", "/assets/12.jpg",
     "/assets/13.jpg", "/assets/14.jpg", "/assets/15.jpg", "/assets/16.jpg",
     "/assets/17.jpg", "/assets/18.jpg", "/assets/20.jpg",
     "/assets/21.jpg", "/assets/22.jpg", "/assets/23.jpg", "/assets/25.jpg",
